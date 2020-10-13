5bba6e6a474f6b5dc5a9d1a5a96af1b6df642f9c
Your Name <you@example.com>
2020-10-13 15:30:31 +0900

    finished lectures

--- covidapp/views.py
+++ covidapp/views.py
@@ -12,23 +12,32 @@ headers = {
 response = requests.request("GET", url, headers=headers).json()
 
 def helloworldview(request):
+  mylist = []
+  noofresults = int(response['results'])
+  for x in range(0, noofresults):
+    mylist.append(response['response'][x]['country'])
   if request.method == 'POST':
     selectedcountry = request.POST['selectedcountry']
     print(selectedcountry)
     noofresults = int(response['results'])
     for x in range(0, noofresults): 
       if selectedcountry == response['response'][x]['country']:
-        new = response['response'][x]['new']
-        active = response['response'][x]['active']
-        critical = response['response'][x]['critical']
-        recovered = response['response'][x]['recovered']
-        total = response['response'][x]['total']
+        new = response['response'][x]['cases']['new']
+        active = response['response'][x]['cases']['active']
+        critical = response['response'][x]['cases']['critical']
+        recovered = response['response'][x]['cases']['recovered']
+        total = response['response'][x]['cases']['total']
         deaths = int(total) - int(active) - int(recovered)
-
-        print(response['response'][x]['cases'])
-  noofresults = int(response['results'])
-  mylist = []
-  for x in range(0, noofresults):
-    mylist.append(response['response'][x]['country'])
+    context ={
+      'selectedcountry': selectedcountry,
+      'mylist': mylist,
+      'new': new,
+      'active': active,
+      'critical': critical,
+      'recovered': recovered,
+      'total': total,
+      "deaths": deaths,
+    }
+    return render(request, 'helloworld.html', context)
   context = {'mylist': mylist}
   return render(request, 'helloworld.html', context)
